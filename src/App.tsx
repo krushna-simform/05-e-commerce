c17017b759c767e94d1eb834241b548c5306768e
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-06 12:51:23 +0530

    FEATURE: add button from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-red-700">ClickCart</h1>
+      <Button className="bg-amber-300 hover:bg-amber-300/80 cursor-pointer text-black">
+        ClickCart
+      </Button>
     </>
   );
 }

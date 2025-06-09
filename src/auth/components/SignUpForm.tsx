a63b36b544e12bd1057b43649a0cf054bdb27dc3
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-09 12:34:54 +0530

    FEATURE: implement sign-up functionality

--- src/auth/components/SignUpForm.tsx
+++ src/auth/components/SignUpForm.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router";
 import { CalendarIcon } from "lucide-react";
+import { useFormik } from "formik";
+import { format } from "date-fns";
 
+import type { Gender, User } from "@/types/user.type";
+import { supabase } from "@/supabase-client";
+import { AddUserSchema } from "@/auth/utils/validations";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -21,196 +26,329 @@ import { Calendar } from "@/components/ui/calendar";
 
 export const SignUpForm = () => {
   const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<Date | undefined>(undefined);
+
+  const formik = useFormik<User>({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      age: 0,
+      gender: "male",
+      contactNumber: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: AddUserSchema,
+    validateOnChange: true,
+    validateOnBlur: true,
+    onSubmit: async (values) => {
+      const newUser: Omit<User, "confirmPassword"> = {
+        firstName: values.firstName.trim(),
+        lastName: values.lastName.trim(),
+        email: values.email.trim(),
+        age: values.age,
+        gender: values.gender,
+        contactNumber: values.contactNumber,
+        password: values.password,
+      };
+
+      try {
+        const { error } = await supabase.auth.signUp(newUser);
+
+        if (error) {
+          alert("An error occurred during signup. Please try again.");
+        } else {
+          alert(
+            "Account created successfully! Please check your email for verification."
+          );
+        }
+      } catch (error) {
+        alert("An error occurred during signup. Please try again.");
+        console.error(error);
+      }
+    },
+  });
+
   return (
-    <div className="flex h-full items-center justify-center">
-      <div className="flex flex-col w-180 gap-6">
-        <p className="text-4xl font-medium my-8 text-center" role="heading">
-          Create an account in{" "}
-          <span className="font-medium bg-gradient-to-r from-[#0792dd] to-indigo-600 bg-clip-text text-transparent">
-            ClickCart
-          </span>
-        </p>
+    <form onSubmit={formik.handleSubmit}>
+      <div className="flex h-full items-center justify-center">
+        <div className="flex flex-col w-180 gap-6">
+          <p className="text-4xl font-medium my-8 text-center" role="heading">
+            Create an account in{" "}
+            <span className="font-medium bg-gradient-to-r from-[#0792dd] to-indigo-600 bg-clip-text text-transparent">
+              ClickCart
+            </span>
+          </p>
 
-        {/* First-name & Last-name */}
-        <div className="flex gap-3">
-          <div className="space-y-3 w-full">
-            <Label htmlFor="first-name" className="text-gray-700">
-              First Name
-            </Label>
-            <Input
-              placeholder="Enter your First Name"
-              id="first-name"
-              className="h-10"
-            />
-          </div>
-          <div className="space-y-3 w-full">
-            <Label htmlFor="last-name" className="text-gray-700">
-              Last Name
-            </Label>
-            <Input
-              placeholder="Enter your Last Name"
-              id="last-name"
-              className="h-10"
-            />
-          </div>
-        </div>
-
-        {/* Email & City */}
-        <div className="flex gap-3">
-          <div className="space-y-3 w-full">
-            <Label htmlFor="email" className="text-gray-700">
-              Email
-            </Label>
-            <Input placeholder="Enter your Email" id="email" className="h-10" />
-          </div>
-          <div className="space-y-3 w-full">
-            <Label htmlFor="city" className="text-gray-700">
-              City
-            </Label>
-            <Input placeholder="Enter your City" id="city" className="h-10" />
-          </div>
-        </div>
-
-        {/* State & Address */}
-        <div className="flex gap-3">
-          <div className="space-y-3 w-full">
-            <Label htmlFor="state" className="text-gray-700">
-              State
-            </Label>
-            <Input placeholder="Enter your State" id="state" className="h-10" />
-          </div>
-          <div className="space-y-3 w-full">
-            <Label htmlFor="address" className="text-gray-700">
-              Address
-            </Label>
-            <Input
-              placeholder="Enter your Address"
-              id="address"
-              className="h-10"
-            />
-          </div>
-        </div>
-
-        {/* Age & Gender */}
-        <div className="flex gap-3">
-          <div className="space-y-3 w-full">
-            <Label htmlFor="age" className="text-gray-700">
-              Age
-            </Label>
-            <Input
-              placeholder="Enter your Age"
-              id="age"
-              className="h-10"
-              type="number"
-            />
-          </div>
-          <div className="space-y-3 w-full">
-            <Label htmlFor="gender" className="text-gray-700">
-              Gender
-            </Label>
-            <Select>
-              <SelectTrigger id="gender" className="w-full py-[18.5px]">
-                <SelectValue placeholder="Gender" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="male">Male</SelectItem>
-                <SelectItem value="female">Female</SelectItem>
-                <SelectItem value="other">Other</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        {/* Contact Number & Profile Image */}
-        <div className="flex gap-3">
-          <div className="space-y-3 w-full">
-            <Label htmlFor="contact-number" className="text-gray-700">
-              Contact Number
-            </Label>
-            <Input
-              placeholder="Enter your Contact Number"
-              id="contact-number"
-              className="h-10"
-              type="number"
-            />
-          </div>
-          <div className="space-y-3 w-full">
-            <Label htmlFor="picture" className="text-gray-700">
-              Profile Picture
-            </Label>
-            <Input
-              id="picture"
-              type="file"
-              className="h-10 text-gray-700 cursor-pointer"
-            />
-          </div>
-        </div>
-
-        {/* BirthDate & Password */}
-        <div className="flex gap-3">
-          <div className="space-y-3 w-full">
-            <Label htmlFor="birth-date" className="text-gray-700">
-              Birth Date
-            </Label>
-            <Popover open={open} onOpenChange={setOpen}>
-              <PopoverTrigger asChild>
-                <Button
-                  type="button"
-                  id="birth-date"
-                  variant="outline"
-                  className="w-full justify-start font-normal cursor-pointer h-10"
-                >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  Select Your Birth Date
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar mode="single" initialFocus />
-              </PopoverContent>
-            </Popover>
-          </div>
-          <div className="space-y-3 w-full">
-            <div className="grid w-full max-w-sm items-center gap-3">
-              <Label htmlFor="password" className="text-gray-700">
-                Password
+          {/* First-name & Last-name */}
+          <div className="flex gap-3">
+            <div className="space-y-3 w-full">
+              <Label htmlFor="firstName" className="text-gray-700">
+                First Name
               </Label>
               <Input
-                placeholder="Enter Password"
-                id="password"
-                type="password"
+                name="firstName"
+                placeholder="Enter your First Name"
+                id="firstName"
+                className="h-10"
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.firstName && formik.errors.firstName && (
+                <p className="text-red-600 text-sm">
+                  {formik.errors.firstName}
+                </p>
+              )}
+            </div>
+            <div className="space-y-3 w-full">
+              <Label htmlFor="lastName" className="text-gray-700">
+                Last Name
+              </Label>
+              <Input
+                name="lastName"
+                placeholder="Enter your Last Name"
+                id="lastName"
+                className="h-10"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.lastName && formik.errors.lastName && (
+                <p className="text-red-600 text-sm">{formik.errors.lastName}</p>
+              )}
+            </div>
+          </div>
+
+          {/* Email & City */}
+          <div className="flex gap-3">
+            <div className="space-y-3 w-full">
+              <Label htmlFor="email" className="text-gray-700">
+                Email
+              </Label>
+              <Input
+                name="email"
+                placeholder="Enter your Email"
+                id="email"
+                className="h-10"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <p className="text-red-600 text-sm">{formik.errors.email}</p>
+              )}
+            </div>
+            <div className="space-y-3 w-full">
+              <Label htmlFor="city" className="text-gray-700">
+                City
+              </Label>
+              <Input placeholder="Enter your City" id="city" className="h-10" />
+            </div>
+          </div>
+
+          {/* State & Address */}
+          <div className="flex gap-3">
+            <div className="space-y-3 w-full">
+              <Label htmlFor="state" className="text-gray-700">
+                State
+              </Label>
+              <Input
+                placeholder="Enter your State"
+                id="state"
+                className="h-10"
+              />
+            </div>
+            <div className="space-y-3 w-full">
+              <Label htmlFor="address" className="text-gray-700">
+                Address
+              </Label>
+              <Input
+                placeholder="Enter your Address"
+                id="address"
+                className="h-10"
+              />
+            </div>
+          </div>
+
+          {/* Age & Gender */}
+          <div className="flex gap-3">
+            <div className="space-y-3 w-full">
+              <Label htmlFor="age" className="text-gray-700">
+                Age
+              </Label>
+              <Input
+                name="age"
+                placeholder="Enter your Age"
+                id="age"
+                className="h-10"
+                type="number"
+                value={formik.values.age}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.age && formik.errors.age && (
+                <p className="text-red-600 text-sm">{formik.errors.age}</p>
+              )}
+            </div>
+            <div className="space-y-3 w-full">
+              <Label htmlFor="gender" className="text-gray-700">
+                Gender
+              </Label>
+              <Select
+                name="gender"
+                value={formik.values.gender}
+                onValueChange={(value) =>
+                  formik.setFieldValue("gender", value as Gender)
+                }
+              >
+                <SelectTrigger id="gender" className="w-full py-[18.5px]">
+                  <SelectValue placeholder="Gender" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="male">Male</SelectItem>
+                  <SelectItem value="female">Female</SelectItem>
+                  <SelectItem value="other">Other</SelectItem>
+                </SelectContent>
+              </Select>
+              {formik.touched.gender && formik.errors.gender && (
+                <p className="text-red-600 text-sm">{formik.errors.gender}</p>
+              )}
+            </div>
+          </div>
+
+          {/* Contact Number & Profile Image */}
+          <div className="flex gap-3">
+            <div className="space-y-3 w-full">
+              <Label htmlFor="contactNumber" className="text-gray-700">
+                Contact Number
+              </Label>
+              <Input
+                name="contactNumber"
+                placeholder="Enter your Contact Number"
+                id="contactNumber"
+                className="h-10"
+                type="number"
+                value={formik.values.contactNumber}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.contactNumber && formik.errors.contactNumber && (
+                <p className="text-red-600 text-sm">
+                  {formik.errors.contactNumber}
+                </p>
+              )}
+            </div>
+            <div className="space-y-3 w-full">
+              <Label htmlFor="picture" className="text-gray-700">
+                Profile Picture
+              </Label>
+              <Input
+                id="picture"
+                type="file"
                 className="h-10 text-gray-700 cursor-pointer"
               />
             </div>
           </div>
-        </div>
 
-        {/* Confirm Password */}
-        <div className="w-1/2 pr-2 space-y-3">
-          <Label htmlFor="confirm-password" className="text-gray-700">
-            Confirm Password
-          </Label>
-          <Input
-            placeholder="Confirm Password"
-            id="confirm-password"
-            type="password"
-            className="h-10 text-gray-700 cursor-pointer"
-          />
-        </div>
+          {/* BirthDate & Password */}
+          <div className="flex gap-3">
+            <div className="space-y-3 w-full">
+              <Label htmlFor="birth-date" className="text-gray-700">
+                Birth Date
+              </Label>
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    type="button"
+                    id="birth-date"
+                    variant="outline"
+                    className="w-full justify-start font-normal cursor-pointer h-10"
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "PPP") : "Select Your Birth Date"}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    initialFocus
+                    selected={date}
+                    onSelect={(date) => {
+                      setDate(date);
+                      setOpen(false);
+                    }}
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div className="space-y-3 w-full">
+              <div className="grid w-full max-w-sm items-center gap-3">
+                <Label htmlFor="password" className="text-gray-700">
+                  Password
+                </Label>
+                <Input
+                  name="password"
+                  placeholder="Enter Password"
+                  id="password"
+                  type="password"
+                  className="h-10 text-gray-700 cursor-pointer"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.password && formik.errors.password && (
+                  <p className="text-red-600 text-sm">
+                    {formik.errors.password}
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
 
-        <div>
-          <Button className="bg-blue-800 text-[16px] w-full py-5 cursor-pointer hover:bg-blue-800/80">
-            Create Account
-          </Button>
-        </div>
-        <div>
-          <p>
-            Already Have An Account?{" "}
-            <Link to="/">
-              <span className="text-blue-600 ml-1.5">Login</span>
-            </Link>
-          </p>
+          {/* Confirm Password */}
+          <div className="w-1/2 pr-2 space-y-3">
+            <Label htmlFor="confirmPassword" className="text-gray-700">
+              Confirm Password
+            </Label>
+            <Input
+              name="confirmPassword"
+              placeholder="confirmPassword"
+              id="confirm-password"
+              type="password"
+              className="h-10 text-gray-700 cursor-pointer"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.confirmPassword &&
+              formik.errors.confirmPassword && (
+                <p className="text-red-600 text-sm">
+                  {formik.errors.confirmPassword}
+                </p>
+              )}
+          </div>
+
+          <div>
+            <Button
+              className="bg-blue-800 text-[16px] w-full py-5 cursor-pointer hover:bg-blue-800/80"
+              type="submit"
+              disabled={!(formik.isValid && formik.dirty)}
+            >
+              Create Account
+            </Button>
+          </div>
+          <div>
+            <p>
+              Already Have An Account?{" "}
+              <Link to="/">
+                <span className="text-blue-600 ml-1.5">Login</span>
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };

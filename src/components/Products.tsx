033f88c56c3b97750085dc6f2985f522397f0d4a
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-10 11:42:09 +0530

    FEATURE: create Sidebar and Product component

---
+++ src/components/Products.tsx
@@ -0,0 +1,27 @@
+import type { Product } from "@/types/product.type";
+import { ProductCard } from "@/components/ProductCard";
+import { Sidebar } from "@/components/Sidebar";
+
+export const Products = ({ product }: { product: Array<Product> }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 ml-65 px-10 mt-12 pt-12 shadow-lg min-h-[80vh]">
+        <div className="flex items-center gap-4">
+          <div className="bg-gradient-to-r from-blue-600 to-indigo-600 h-10 w-5 rounded-l-sm"></div>
+          <p
+            className="bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent text-2xl w-50 font-medium"
+            role="heading"
+          >
+            Our Products
+          </p>
+        </div>
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-10 py-7">
+          {product.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,46 @@
+// components/Sidebar.tsx
+import { useSort } from "@/hooks/useSort";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import type { SortOption } from "@/types/sort.type";
+
+const sortOptionLabels: Record<string, string> = {
+  none: "No Sorting",
+  "name-asc": "Name: A to Z",
+  "name-desc": "Name: Z to A",
+  "price-asc": "Price: Low to High",
+  "price-desc": "Price: High to Low",
+};
+
+export const Sidebar = () => {
+  const { sortOption, setSortOption } = useSort();
+
+  return (
+    <div className="w-65 flex flex-col items-center fixed left-0 top-0 h-full mt-17 pt-10 ">
+      <h2 className="text-lg font-semibold mb-2">Sort By</h2>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="cursor-pointer">
+            {sortOptionLabels[sortOption] ?? "Select Sorting"}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {Object.entries(sortOptionLabels).map(([key, label]) => (
+            <DropdownMenuItem
+              key={key}
+              onClick={() => setSortOption(key as SortOption)}
+              className="cursor-pointer"
+            >
+              {label}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};

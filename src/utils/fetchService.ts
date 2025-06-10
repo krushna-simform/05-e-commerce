f576a7fce51cfe1c89a79696a93e0a42b82be959
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-10 11:34:40 +0530

    FEATURE: create fetchService utility function

---
+++ src/utils/fetchService.ts
@@ -0,0 +1,13 @@
+import type { ProductResponse } from "@/types/product.type";
+
+export const fetchProducts = async (): Promise<ProductResponse> => {
+  const res = await fetch("https://dummyjson.com/products?skip=25&limit=100");
+
+  if (!res.ok) {
+    console.error("API error:", res.status, res.statusText);
+    throw new Error("Error occurred while fetching the Products.");
+  }
+
+  const data = await res.json();
+  return data;
+};

f56ea9db518a4d6224ebd902cf794e32aacb5dd4
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-12 12:47:16 +0530

    FEATURE: made changes in sidebar ui

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Menu } from "lucide-react";
+import { IoClose } from "react-icons/io5";
+
 import { useSort } from "@/hooks/useSort";
 import {
   DropdownMenu,
@@ -28,7 +30,8 @@ export const Sidebar = () => {
   return (
     <div>
       <Button
-        className="md:hidden h-10 fixed bottom-4 left-4 z-50 bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-500 hover:to-indigo-500 rounded-full cursor-pointer"
+        className="md:hidden h-10 fixed bottom-4 left-4 z-50 bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-500 hover:to-indigo-500 rounded-full cursor-pointer 
+        "
         onClick={toggleSidebar}
       >
         <Menu />
@@ -36,7 +39,7 @@ export const Sidebar = () => {
 
       <div
         className={cn(
-          "md:flex flex-col items-center fixed left-0 top-0 h-full mt-[69px] pt-10 z-40 bg-white transition-transform duration-300",
+          "md:flex flex-col items-center fixed left-0 top-0 h-full mt-[69px] pt-20 md:pt-10 z-40 bg-white transition-transform duration-300",
           {
             "transform-none w-50 md:border-none border": isSidebarOpen,
             "transform -translate-x-full w-50": !isSidebarOpen,
@@ -44,7 +47,13 @@ export const Sidebar = () => {
           }
         )}
       >
-        <div className="flex flex-col items-center w-full h-full md:mt-10">
+        <div
+          className="flex flex-col items-center w-full h-full md:mt-10"
+          onClick={toggleSidebar}
+        >
+          <button className="flex md:hidden absolute top-0 right-0 p-2 cursor-pointer text-xl">
+            <IoClose />
+          </button>
           <h2 className="text-lg font-semibold mb-4 text-center">Sort By</h2>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>

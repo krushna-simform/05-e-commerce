f95011c452620f7f5ff76dd6fd483ccbe32972da
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-18 16:36:56 +0530

    FEATURE: create auth types

---
+++ src/types/auth.type.ts
@@ -0,0 +1,54 @@
+interface SignupData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  contactNumber: string;
+  password: string;
+}
+
+interface UserData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: "ADMIN" | "USER" | string;
+  contactNumber: string;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+  profileCompleted: boolean;
+  teacher?: null | unknown;
+  student?: null | unknown;
+}
+interface SignupSigninResponseSuccess {
+  status: "success";
+  message: string;
+  data: {
+    user: UserData;
+    accessToken: string;
+    refreshToken: string;
+  };
+}
+
+interface SignupSignupResponseError {
+  status: "error";
+  message: string;
+}
+
+interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+type SignupSignupResponse =
+  | SignupSigninResponseSuccess
+  | SignupSignupResponseError;
+
+export type {
+  SignupData,
+  UserData,
+  SignupSigninResponseSuccess,
+  SignupSignupResponseError,
+  SignupSignupResponse,
+  AuthTokens,
+};
